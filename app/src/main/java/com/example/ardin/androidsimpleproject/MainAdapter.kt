2b3ee089e4775540e2a362880f157544625bcb12
Muhtarudin Siregar ツ <redcar.studious@gmail.com>
2018-01-29 15:19:24 +0700

    Fix heigh recyler with wrap content

--- app/src/main/java/com/example/ardin/androidsimpleproject/MainAdapter.kt
+++ app/src/main/java/com/example/ardin/androidsimpleproject/MainAdapter.kt
@@ -4,13 +4,17 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import com.example.ardin.androidsimpleproject.R.id.textView_video_title
 
 /**
  * Created by ardin on 29/01/18.
  */
 class MainAdapter : RecyclerView.Adapter<CustomViewHolder>() {
+    val videoList = listOf<String>("Jumanji", "Dilan")
+
     override fun getItemCount(): Int {
-        return 2
+        return videoList.size
     }
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): CustomViewHolder {
@@ -19,11 +23,11 @@ class MainAdapter : RecyclerView.Adapter<CustomViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: CustomViewHolder?, position: Int) {
-
+        val data = holder?.view?.findViewById<TextView>(textView_video_title)
+        data?.text = videoList[position]
     }
 }
 
-class CustomViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-    val card = view
+class CustomViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
 
 }
