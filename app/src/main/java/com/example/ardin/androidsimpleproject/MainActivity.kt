64b20c058d2b0e81aaa08fec277b086d8ed20729
Muhtarudin Siregar ツ <redcar.studious@gmail.com>
2018-01-29 09:26:13 +0700

    Using kotinx for viewHolder

--- app/src/main/java/com/example/ardin/androidsimpleproject/MainActivity.kt
+++ app/src/main/java/com/example/ardin/androidsimpleproject/MainActivity.kt
@@ -1,14 +1,16 @@
 package com.example.ardin.androidsimpleproject
 
 import android.content.Context
-import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.ListView
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_row_main.view.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,27 +19,64 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val listView = findViewById<ListView>(R.id.main_listview)
-        val redColor = Color.parseColor("#FF0000")
-        listView.setBackgroundColor(redColor)
 
-        listView.adapter = MyCustomAdapter(this) // adapter telling my list what to render
+        listView.adapter = MyCustomAdapter() // adapter telling my list what to render
 
     }
 
-    private class MyCustomAdapter(context: Context) : BaseAdapter() {
-        private val mContext: Context
+    private class MyCustomAdapter() : BaseAdapter() {
+//        private val mContext: Context
+        private val names = arrayListOf<String>(
+                "Donald Trump", "Jokowi", "Steve Jobs", "Steve Aoki",
+                "Donald Trump", "Jokowi", "Steve Jobs", "Steve Aoki",
+                "Donald Trump", "Jokowi", "Steve Jobs", "Steve Aoki",
+                "Donald Trump", "Jokowi", "Steve Jobs", "Steve Aoki",
+                "Donald Trump", "Jokowi", "Steve Jobs", "Steve Aoki",
+                "Donald Trump", "Jokowi", "Steve Jobs", "Steve Aoki",
+                "Donald Trump", "Jokowi", "Steve Jobs", "Steve Aoki",
+                "Donald Trump", "Jokowi", "Steve Jobs", "Steve Aoki",
+                "Donald Trump", "Jokowi", "Steve Jobs", "Steve Aoki",
+                "Donald Trump", "Jokowi", "Steve Jobs", "Steve Aoki",
+                "Donald Trump", "Jokowi", "Steve Jobs", "Steve Aoki"
+        )
 
-        init {
-            this.mContext = context
-        }
+
+//        init {
+//            this.mContext = context
+//        }
 
         //responsible for rendering out each row
         override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-            val textView = TextView(mContext)
+            val rowMain: View
 
-            textView.text = "Lorem ipsum dolor"
-            return textView
+
+            if (convertView == null) {
+                val layoutInflater = LayoutInflater.from(parent!!.context)
+                rowMain = layoutInflater.inflate(R.layout.activity_row_main, parent, false)
+
+//                val nTextView = rowMain.name_textView
+//                val pTextView = rowMain.position_textview
+//
+//
+//                val positionTextView = rowMain.findViewById<TextView>(R.id.position_textview)
+//                val nameTextView = rowMain.findViewById<TextView>(R.id.name_textView)
+
+                val viewHolder = ViewHolder(rowMain.name_textView, rowMain.position_textview)
+
+                rowMain.tag = viewHolder
+            } else {
+                rowMain = convertView
+            }
+
+            val viewHolder = rowMain.tag as ViewHolder
+
+            viewHolder.nameTextView.text = names.get(position)
+            viewHolder.positionTextView.text = "Row Number : $position"
+
+            return rowMain
         }
+
+        private class ViewHolder(val nameTextView: TextView, val positionTextView: TextView)
 
 
         override fun getItem(position: Int): Any {
@@ -49,8 +88,9 @@ class MainActivity : AppCompatActivity() {
         }
 
         override fun getCount(): Int {
-            return 5
+            return names.size
         }
 
     }
+
 }
---
+++ app/src/main/java/com/example/ardin/androidsimpleproject/row_main.kt
@@ -0,0 +1,12 @@
+package com.example.ardin.androidsimpleproject
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class row_main : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_row_main)
+    }
+}
