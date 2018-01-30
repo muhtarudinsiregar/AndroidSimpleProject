bc03e15ddaea4a5c42523fa5aa5d1f70a4f5ae42
Muhtarudin Siregar ツ <redcar.studious@gmail.com>
2018-01-30 10:44:08 +0700

    Added OkHttp + fix permission internet in manifest

--- app/src/main/java/com/example/ardin/androidsimpleproject/MainActivity.kt
+++ app/src/main/java/com/example/ardin/androidsimpleproject/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.*
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,5 +15,27 @@ class MainActivity : AppCompatActivity() {
 
         recyclerView_main.layoutManager = LinearLayoutManager(this)
         recyclerView_main.adapter = MainAdapter()
+
+        fetchJson()
+    }
+
+    fun fetchJson() {
+        val url = "https://api.letsbuildthatapp.com/youtube/home_feed"
+        val request = Request.Builder().url(url).build()
+
+        val client = OkHttpClient()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call?, e: IOException?) {
+                println("Failed to execute request")
+            }
+
+            override fun onResponse(call: Call?, response: Response?) {
+                val body = response?.body()?.string()
+                println(body)
+            }
+
+        })
+
     }
 }
